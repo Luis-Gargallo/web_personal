a0889224ca42e356f48fc6a73a446bea5cce6699
Luis Gargallo <lgargallo@localhost.localdomain>
2020-12-07 13:52:17 -0600

    eliminar carpeta

--- server/routers/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { refreshAccessToken } = require("../controllers/auth");
-const AuthController = require('../controllers/auth');
-
-const api = express.Router();
-
-api.post("/refresh-access-token", AuthController.refreshAccessToken);
-
-module.exports = api;
--- server/routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const UserController = require("../controllers/user");
-
-const api = express.Router();
-
-api.post("/sign-up", UserController.signUp);
-api.post("/sign-in", UserController.signIn);
-
-module.exports = api;
